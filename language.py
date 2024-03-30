0eff5e8414b9d4dfe37652d71651f7a5de4da867
Chihab Derghal <chihabderghal1@gmail.com>
2024-03-30 23:56:47 +0100

    Add Spanish and French functions

--- language.py
+++ language.py
@@ -1,10 +1,25 @@
+# this was written in the main branch
+def Spanish() -> None: 
+    print("Hola, Mundo")
+
+
+# this was written in the main branch
+def French() -> None: 
+    print("Bonjour, Le Monde")
+
+
+# this was written in the main branch
 def English() -> None: 
     print("Hello, World")
 
 
+# this was written in the main branch
 def main() -> None:
     English()
+    French()
+    Spanish()
 
 
+# this was written in the main branch
 if __name__ == "__main__":
     main()
